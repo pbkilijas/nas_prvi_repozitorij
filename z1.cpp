dc618996369eda3c0ab33955e89c8400c622f32b
pbkilijas <167958999+pbkilijas@users.noreply.github.com>
2024-04-24 08:27:17 +0200

    u z1 dodali NZD i testni main program

--- z1.cpp
+++ z1.cpp
@@ -2,8 +2,29 @@
 
 using namespace std;
 
+int nzd(int a, int b) {
+    int minimum = a;
+    if(b < minimum) {
+        minimum = b;
+    }
+
+    int djelioc = 1;
+
+    for(int i = 2; i <= minimum; i++) {
+        if(a % i == 0 && b % i == 0) {
+            djelioc = i;
+        }
+    }
+
+    return djelioc;
+}
+
 int main() {
-    cout << "Ovo je nas main program" << endl;
+    int a, b;
+    cout << "Unesite a i b: ";
+    cin >> a >> b;
+
+    cout << "NZD(" << a << ", " << b << ") = " << nzd(a, b) << endl;
 
     return 0;
 }
